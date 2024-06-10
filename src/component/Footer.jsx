411d3a72443c8f562050158bd4278081d8e550b1
Mohammed Sirajudeen <160144085+mohammedsira@users.noreply.github.com>
2024-06-10 18:43:57 +0530

    Update Footer.jsx

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -5,7 +5,7 @@ function Footer() {
         <footer className="py-5 mt-5 bg-dark">
             <div className="container text-center">
                 <h6 className="lead fw-normal text-center text-white-50 mb-0">
-                    GROCERY AND ORGANIC FOOD SHOP
+                    TREND YOUR STYLISH COOL FASHION
                 </h6>
                 <br />
                 <p className="m-0 text-center text-white">
